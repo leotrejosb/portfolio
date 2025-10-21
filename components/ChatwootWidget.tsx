461075e158ff5f0d9b1da74e456029e63a4aef95
leotrejosb <leonardo.trejos@utp.edu.co>
2025-10-21 16:39:26 -0500

    Try chatbot page 2

--- components/ChatwootWidget.tsx
+++ components/ChatwootWidget.tsx
@@ -1,92 +1,60 @@
 'use client';
 
-import { useEffect } from 'react';
 import Script from 'next/script';
 
 export default function ChatwootWidget() {
-  useEffect(() => {
-    // Observador para detectar cuando el widget se carga
-    const observer = new MutationObserver(() => {
-      const holder = document.getElementById('cw-widget-holder');
-      const iframe = document.getElementById('chatwoot_live_chat_widget') as HTMLIFrameElement;
-      
-      if (holder && iframe) {
-        // Remover la clase que oculta el widget
-        holder.classList.remove('woot--hide');
-        
-        // Forzar visibilidad del iframe
-        iframe.style.visibility = 'visible';
-        iframe.style.display = 'block';
-        
-        // Detener el observador una vez que el widget esté visible
-        observer.disconnect();
-      }
-    });
-
-    // Observar cambios en el body
-    observer.observe(document.body, {
-      childList: true,
-      subtree: true,
-    });
-
-    // Limpiar el observador al desmontar
-    return () => observer.disconnect();
-  }, []);
-
-  const handleLoad = () => {
-    // Intentar mostrar el widget después de un pequeño delay
-    setTimeout(() => {
-      const holder = document.getElementById('cw-widget-holder');
-      const iframe = document.getElementById('chatwoot_live_chat_widget') as HTMLIFrameElement;
-      
-      if (holder) {
-        holder.classList.remove('woot--hide');
-      }
-      
-      if (iframe) {
-        iframe.style.visibility = 'visible';
-        iframe.style.display = 'block';
-      }
-    }, 500);
-  };
-
   return (
     <Script 
-      id="chatwoot-widget" 
+      id="chatwoot-widget"
       strategy="afterInteractive"
-      onLoad={handleLoad}
-    >
-      {`
-        (function(d,t) {
-          var BASE_URL="https://zzchatwootzz.cerebria.co";
-          var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-          g.src=BASE_URL+"/packs/js/sdk.js";
-          g.async = true;
-          s.parentNode.insertBefore(g,s);
-          g.onload=function(){
-            window.chatwootSDK.run({
-              websiteToken: 'MWdFbwGURy8LGaeD96hst8BQ',
-              baseUrl: BASE_URL
-            });
-            
-            // Forzar visibilidad después de inicializar
-            setTimeout(function() {
-              var holder = document.getElementById('cw-widget-holder');
-              var iframe = document.getElementById('chatwoot_live_chat_widget');
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.chatwootSettings = {
+            "position": "right",
+            "type": "expanded_bubble",
+            "launcherTitle": "Chatea con nosotros",
+            "hideMessageBubble": false
+          };
+          
+          (function(d,t) {
+            var BASE_URL="https://zzchatwootzz.cerebria.co";
+            var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=BASE_URL+"/packs/js/sdk.js";
+            g.async = true;
+            s.parentNode.insertBefore(g,s);
+            g.onload=function(){
+              window.chatwootSDK.run({
+                websiteToken: '4VcyLAofaVmTTCWxjincMHrS',
+                baseUrl: BASE_URL
+              });
               
-              if (holder) {
-                holder.classList.remove('woot--hide');
-                holder.style.display = 'block';
-              }
-              
-              if (iframe) {
-                iframe.style.visibility = 'visible';
-                iframe.style.display = 'block';
-              }
-            }, 1000);
-          }
-        })(document,"script");
-      `}
-    </Script>
+              // Forzar visibilidad de la burbuja flotante
+              setTimeout(function() {
+                var holder = document.querySelector('.woot-widget-holder');
+                var bubble = document.querySelector('.woot-widget-bubble');
+                var bubbleHolder = document.querySelector('.woot--bubble-holder');
+                
+                // Remover clases que ocultan
+                if (holder) {
+                  holder.classList.remove('woot--hide');
+                }
+                
+                if (bubbleHolder) {
+                  bubbleHolder.classList.remove('woot--hide');
+                }
+                
+                // Hacer visible la burbuja
+                if (bubble) {
+                  bubble.classList.remove('woot--hide');
+                  bubble.style.visibility = 'visible';
+                  bubble.style.display = 'flex';
+                  bubble.style.opacity = '1';
+                }
+              }, 1500);
+            }
+          })(document,"script");
+        `
+      }}
+    />
   );
 }
